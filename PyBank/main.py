b58c96bb8cb7b03c14cd2ad420bd3c32d0d0af8c
Rhea2501 <rheakhanna100@gmail.com>
2024-06-21 21:38:54 +1000

    Add analysis scripts and results for PyBank and PyPoll

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,24 +1,17 @@
 import csv
 import os
 
+
 # Define the csv file path
-csvpath = 'C:/Users/rheak/Documents/Bootcamp Assignment Resources/Starter_Code (3)/Starter_Code/PyBank/Resources/budget_data.csv'
-
-
+# csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+csvpath = os.path.join('PyBank\Resources','budget_data.csv')
+# OR 
+#csvpath = 'C:/Users/rheak/Documents/Bootcamp Assignment Resources/Starter_Code (3)/Starter_Code/PyBank/Resources/budget_data.csv'
 
 # Define the output folder 
-output_folder = 'C:/Users/rheak/Documents/Bootcamp Assignment Submission/Challenge 3 - Wk 3/Python-Challenge/PyBank/Analysis'
+output_folder = 'PyBank\Analysis'
 output_file = os.path.join(output_folder, 'financial_analysis.txt')
 
-
- # Reading the csv file
-with open(csvpath) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
-    print(csvreader)
-    
-    csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
- 
 
 # Initialise variables
 total_months = 0
@@ -26,36 +19,58 @@ net_total = 0
 changes = []
 dates = []
 previous_profit_loss = None  # Initialising previous_profit_loss as None
+average_change = 0
+max_increase = 0
+max_decrease = 0
+max_increase_date = 0
+max_decrease_date = 0
 
 
-# Process each row
-for row in csvreader:
-    date = row[0]
-    profit_loss = int(row[1])
-        
-# Count total months 
-    total_months = total_months + 1
-        
-    #Sum of profit/losses
-    net_total = net_total + profit_loss
-        
-    # Calculate changes
-    if previous_profit_loss is not None:
-        change = profit_loss - previous_profit_loss
-        changes.append(change)
-        dates.append(date)
-    
-    previous_profit_loss = profit_loss  
+# Reading the csv file
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    print(csvreader)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+ 
 
+    # Process each row
+    for row in csvreader:
+        date = row[0]
+        profit_loss = int(row[1])
+        
+        # Count total months 
+        total_months = total_months + 1
+        
+        #Sum of profit/losses
+        net_total = net_total + profit_loss
+        
+        # Calculate changes
+        if previous_profit_loss is not None:
+            change = profit_loss - previous_profit_loss
+            changes.append(change)
+            dates.append(date)
+        
+
+        # Update previous profit/loss    
+        previous_profit_loss = profit_loss      
 
 
 # Calculate average change, greatest increase, and greatest decrease
-average_change = round(sum(changes) / len(changes), 2)
-max_increase = max(changes)
-max_decrease = min(changes)
-max_increase_date = dates[changes.index(max_increase)]
-max_decrease_date = dates[changes.index(max_decrease)]
+if changes:   
+    average_change = round(sum(changes) / len(changes), 2)
+    max_increase = max(changes)
+    max_decrease = min(changes)
+    max_increase_date = dates[changes.index(max_increase)]
+    max_decrease_date = dates[changes.index(max_decrease)]
 
+else:
+    average_change = 0
+    max_increase = 0
+    max_decrease = 0
+    max_increase_date = ""
+    max_decrease_date = ""
+    
 
 # Prepare the results
 results = (
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,24 +1,19 @@
 import csv
 import os
 
-# Define the CSV file path
-csvpath = 'C:/Users/rheak/Documents/Bootcamp Assignment Resources/Starter_Code (3)/Starter_Code/PyPoll/Resources/election_data.csv'
+# Define the csv file path
+# csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+csvpath = os.path.join('PyPoll\Resources','election_data.csv')
+# OR 
+#csvpath = 'C:/Users/rheak/Documents/Bootcamp Assignment Resources/Starter_Code (3)/Starter_Code/PyBank/Resources/budget_data.csv'
 
-
-# Read the CSV file
-with open(csvpath) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
-    csv_header = next(csvreader)  # Skip the header row
-
-
-# Export the results to a text file
-output_folder = 'C:/Users/rheak/Documents/Bootcamp Assignment Submission/Challenge 3 - Wk 3/Python-Challenge/PyPoll/Analysis'
+# Define the output folder 
+output_folder = 'PyPoll\Analysis'
 output_file = os.path.join(output_folder, 'election_results.txt')
 
-# Create the analysis folder if it doesn't exist
-if not os.path.exists(output_folder):
-    os.makedirs(output_folder)
 
+# OR Define the CSV file path
+# csvpath = 'C:/Users/rheak/Documents/Bootcamp Assignment Resources/Starter_Code (3)/Starter_Code/PyPoll/Resources/election_data.csv'
 
 
 # Initialize variables
@@ -28,8 +23,16 @@ winner = ""
 max_votes = 0
 
 
+# Read the CSV file
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    print(csvreader)
+    csv_header = next(csvreader)
+    print()
+
+
     # Process each row
-for row in csvreader:
+    for row in csvreader:
         total_votes = total_votes + 1  # Count total votes explicitly
         candidate = row[2]  # Get the candidate's name
         
